cacfdaaa66cbefde61f0f53ca02831ae839372cf
Ali Fnaiech <alifnaiech@gmail.com>
2021-01-07 15:37:37 +0100

    game monster

---
+++ assets/scripts/app.js
@@ -0,0 +1,55 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+adjustHealthBars(chosenMaxLife);
+
+
+function endRound(){
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+        alert('YOU WON!');
+    }
+    else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+        alert('YOU LOST');
+    }
+    else if(currentMonsterHealth <= 0 &&  currentPlayerHealth <= 0){
+        alert('YOU HAVE A DRAW!');
+    }
+}
+
+function attackMonster(mode){
+    let maxDamage = '';
+    if(mode === 'ATTACK'){
+        maxDamage = ATTACK_VALUE;
+    }else if(mode === 'STRONG_ATTACK'){
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    endRound();
+}
+
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK')
+}
+
+function healPlayerHandler(){
+    increasePlayerHealth(HEAL_VALUE);
+    endRound();
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
